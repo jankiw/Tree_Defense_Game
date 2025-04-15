e37434d849534263108e56efff63d026ee96615e
jankiw <jan.kiwlenko@gmail.com>
2025-04-15 12:53:26 +0200

    square tilemap

--- tree-defense-game/src/test_enemy.gd
+++ tree-defense-game/src/test_enemy.gd
@@ -1,6 +1,6 @@
 extends CharacterBody2D
 
-@export var speed = 100
+@export var speed = 50
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
