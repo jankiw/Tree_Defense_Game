c32f5d8969a7e61dfe1cc1dd139b601d1ede8bf6
jankiw <jan.kiwlenko@gmail.com>
2025-09-24 02:44:33 +0200

    additional camera features

--- tree-defense-game/src/test_level.gd
+++ tree-defense-game/src/test_level.gd
@@ -1,5 +1,9 @@
 extends Node
 
+var camera_left = 0;
+var camera_right = 0;
+var camera_up = 0;
+var camera_down = 0;
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
